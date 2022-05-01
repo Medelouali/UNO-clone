78b400bbb73e630515c7c67adbf93d73f62610c4
unknown <chaimae.lekhzoumy@gmail.com>
2022-05-01 15:46:47 +0000

    defined player class

---
+++ utilities/classes/object/player/Player.py
@@ -0,0 +1,23 @@
+class Player:
+    def _init_(self, ID, hand, playerActive, score):
+        self.ID = ID
+        self.hand = []
+        self.playerActive = False
+        self.score = 0
+    def isActive(self):
+        return self.playerActive
+    def throwCard(self, playedCards, cardToPlay):
+        if self.playerActive == True:
+            card = self.hand.pop(cardToPlay)"""temporary variable to hold the popped card"""
+            playedCards.push(card)
+    def screamUno(self):
+        if self.playerActive == True:
+            if self.hand.cardinal==1:
+            """print("UNO!") we can either do it automatically or
+            canClickUno= True this makes a button on screen clickable then the player can either click it or not"""
+    
+        
+            
+            
+            
+        
