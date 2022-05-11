1c0dd5fbdfdfd88d65af43516181fdf1a4feaa1c
GhitaBenchekroun <101916271+GhitaBenchekroun@users.noreply.github.com>
2022-05-11 09:26:03 +0100

    Update skip.py

--- specialCards/skip.py
+++ specialCards/skip.py
@@ -1,7 +1,7 @@
 def skip (game , player =[]):
 	temp = game.getState("rotation")
  	game.setState("rotation",game.getState("rotation")*2)
-    if (game.getState("activePlayer") == len(players)-1 or game.getState("activePlayer") == len(players)-2) and rotation == 2:
+    if rotation == 2:
     	if game.getState("activePlayer") == len(players)-1: game.setState("activePlayer",1)
         if game.getState("activePlayer") == len(players)-2: game.setState("activePlayer",0)
     else : 
