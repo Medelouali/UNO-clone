0b4153da73430b6f6087686e4b42004b499fd058
KaoutharKabiri <101905930+KaoutharKabiri@users.noreply.github.com>
2022-05-15 18:35:52 +0100

    Update variables.py

--- utilities/variables/variables.py
+++ utilities/variables/variables.py
@@ -1,3 +1,4 @@
 unoColors=[ "Green", "Blue", "Red", "Yellow"]
 unoNumberRange=list(range(0,10)) #rang des number-cards (0-9 cards)
-coloredType=["Skip", "Reverse", "Draw 2", "Draw 4", "Wild"]
+coloredType=["Skip", "Reverse", "Draw 2", "Draw 4"]
+#Wild card is not a colored type
