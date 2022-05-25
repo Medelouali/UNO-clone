47542510a83281c7f477ba3c5b10a189f5572809
A-E-Mourabet <elmourabet17@gmail.com>
2022-05-25 00:45:45 +0000

    --init-- problems?

--- tests/GameStartMenu/ImportTest.py
+++ tests/GameStartMenu/ImportTest.py
@@ -1,2 +1,11 @@
 import pygame,sys
-from utilities.classes.game.Game import Game
+pygame.init()
+from MainMenu import MainMenu
+pygame.display.set_caption('UNO')
+# setting the variables 
+ActMenu=""#to mark the menu
+RectDic ={}# to mark the existing Rect's
+
+MainMenu(ActMenu,RectDic)
+
+
