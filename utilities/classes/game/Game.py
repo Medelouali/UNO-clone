68b686f709fda16d652cfb32fcf199da148b8ed7
Medelouali <Mohammedelouali1999@gmail.com>
2022-05-30 13:31:37 -0700

    testing

--- utilities/classes/game/Game.py
+++ utilities/classes/game/Game.py
@@ -183,6 +183,7 @@ class Game:
         # to limit the width of the card
         if(cardWith>100): 
             cardWith=100
+            moveBy+=(Game.screenWidth-2*handMargin-cardWith*len_t-cardMargin*len_t)/2
         for i in range(len_t):
             hand[i].setPosition([moveBy, Game.screenHeight-100]).setDimentions((cardWith, 100)).add()
             moveBy+=cardMargin+cardWith
