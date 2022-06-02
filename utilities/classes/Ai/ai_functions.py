09c2bc47d1a2f9400485fae9d29240f532289db1
A-E-Mourabet <elmourabet17@gmail.com>
2022-06-02 14:25:18 +0000

    Menus Ver.6

--- utilities/classes/Ai/ai_functions.py
+++ utilities/classes/Ai/ai_functions.py
@@ -6,3 +6,17 @@ def determine_inhibit(self,players,lastPlayedCard):
                 if self.getHand()[i].type in ["Skip", "Reverse", "Draw","Draw4"] and self.getHand()[i].getColor() ==lastPlayedCard.getColor():
                    return i
             return -1
+
+# function defining the type of hand [NormalOnly , MixedCards , SpecialOnly]
+def TypeHand(self , playableCards ) :
+    Normal=False
+    Special=False
+    hand=self.getHand
+    for i in range (len(playableCards)) :
+        if (Normal==True and Special==True):break
+        if (self.hand[playableCards[i]].type =="Normal") : Normal=True 
+        if (self.hand[playableCards[i]].type in ["Skip", "Reverse", "Draw","Draw4"]) : Special=True
+    if(Normal==True and Special==True) : print("MixedCards")
+    if(Normal==True and Special==False) : print ("NormalOnly")
+    if(Normal==False and Special==True) : print ("SpecialOnly")
+    if(Normal==False and Special==False) : print ("For Error")
