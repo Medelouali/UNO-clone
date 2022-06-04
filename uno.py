40fbec07642aa90f3eb860ff66539a17e8c84195
Medelouali <Mohammedelouali1999@gmail.com>
2022-06-04 07:05:20 -0700

    setting up the socket connections

--- uno.py
+++ uno.py
@@ -1,6 +1,9 @@
 from utilities.classes.game.Game import Game
 import sys
 
+# run this file with a flag as command line argument if you want it to be a server
+# otherwise just ignore this comment
+
 if(__name__ == '__main__'):
     argumentList = sys.argv[1:]
     game = Game(False if len(argumentList) > 0 else True)
