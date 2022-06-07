d4413e8e6d503357520fdbed793831c5b8fc62cb
Medelouali <Mohammedelouali1999@gmail.com>
2022-06-07 14:13:34 -0700

    setup

--- uno.py
+++ uno.py
@@ -1,11 +1,6 @@
-# from menus.main import main
-from utilities.classes.game.Game import Game
-from utilities.sockets.network import Network
-
+from menus.main import main
 
 if(__name__ == '__main__'):
-    # menu_label = "" #to mark the menu 
-    # rectangles ={} # to mark the existing Rect's
-    # main(menu_label, rectangles)
-    game = Game(Network())
-    game.run()
+    menu_label = "" #to mark the menu 
+    rectangles ={} # to mark the existing Rect's
+    main(menu_label, rectangles)
---
+++ uno_dev.py
@@ -0,0 +1,7 @@
+from utilities.classes.game.Game import Game
+from utilities.sockets.network import Network
+
+# run this for development
+if(__name__ == '__main__'):
+    game = Game(Network())
+    game.run()
